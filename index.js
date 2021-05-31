926d6d2bcf79a8e2cc46bbb8e819e885e0701864
Gandalf <dylangreen034@gmail.com>
2021-05-31 18:38:02 -0500

    html file created

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const team = teamBuild => {
             break
 
             case 'finish building my team':
+            html()
             console.log('team finished!')
             break
         }
@@ -75,7 +76,7 @@ const managerQuestions = () => {
                 }
             }
         }
-    ])    
+    ])
 }
 
 const internQuestions = () => {
@@ -128,16 +129,14 @@ const internQuestions = () => {
                 }
             }
         }
-    ])
+    ]).then(team)
 }
 
-const employeeQuestions = (
-
-) => {
+const employeeQuestions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
+            name: 'engineerName',
             message: 'what is your engineers name?',
             validate: enpName => {
                 if (enpName) {
@@ -149,7 +148,7 @@ const employeeQuestions = (
         },
         {
             type: 'input',
-            name: 'id',
+            name: 'engineerId',
             message: 'what is your employees id?',
             validate: enpId => {
                 if (enpId) {
@@ -161,7 +160,7 @@ const employeeQuestions = (
         },
         {
             type: 'input',
-            name: 'email',
+            name: 'engineerEmail',
             message: 'what is your employees email?',
             validate: enpEmail => {
                 if (enpEmail) {
@@ -171,14 +170,79 @@ const employeeQuestions = (
                 }
             }
         },
-        // {
-        //     type: 'confirm',
-        //     name: 'confirmNewEmployee',
-        //     message: 'Would you like to enter another employee?',
-        //     default: false
-        // }
-    ])
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'What is your engineers github username?',
+            validate: enpGithub => {
+                if (enpGithub) {
+                    return true;
+                } else {
+                    console.log('Please enter a email')
+                }
+            }
+        }
+    ]).then(team)
 }
 
+const html = data => {
+    const inHTML = `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+    <header>
+        <nav class="navbar navbar-danger bg-danger mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+    </header>
+  <body>  
+  <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    ${data.managerName}
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${data.managerId}</li>
+    <li class="list-group-item">${data.managerEmail}</li>
+    <li class="list-group-item">${data.officeNumber}</li>
+  </ul>
+</div>
+
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()}</h3>
+    </footer>
+  </body>
+  </html>`;
+  fs.writeFile('./dist/index.html', inHTML, err => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve({
+      ok: true,
+      message: 'File created!'
+    });
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+           reject(err);
+           return;
+         }
+         resolve({
+           ok: true,
+           message: 'File created!'
+         });
+       });
+    })
+}
 managerQuestions()
 .then(team)
